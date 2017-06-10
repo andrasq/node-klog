48a902148a39416abf4772430718229537b93c23
Andras <andras@kinvey.com>
2017-06-10 12:46:14 -0400

    rearrange and improve benchmarks

--- test/benchmark.js
+++ test/benchmark.js
@@ -3,14 +3,15 @@ var aflow = require('aflow');
 var fs = require('fs');
 var request = require('request');
 var http = require('http');
-var httpAgent = new http.Agent({ keepAlive: true, maxSockets: 20 });
+var httpAgent = new http.Agent({ keepAlive: true, maxSockets: 40 });
 var Url = require('url');
 var qhttp = require('qhttp');
 var Fputs = require('qfputs');
 var qprintf = require('qprintf');
 var qrpc = require('qrpc');
+var qtimeit = require('qtimeit');
 
-var klog = require('../lib/klog-server');
+var klog = require('../');
 
 var x190 = "x".repeat(190);
 var loglines = new Array();
@@ -41,6 +42,139 @@ if (cluster.isMaster) {
 else {
 
     var client = qrpc.connect(4245, 'localhost', function(socket) {
+        socket.setNoDelay(true);
+        client.call('logname', 'testlog');
+    });
+
+    var klogClient = klog.createClient('testlog', { qrpcPort: 4245, host: 'localhost' }, function(c) {
+    });
+
+    qtimeit.bench.timeGoal = 2.0;
+    qtimeit.bench.opsPerTest = 100;
+    qtimeit.bench({
+        'pause': function(done) {
+            setTimeout(done, 100);
+        },
+
+/**
+        'qrpc.connect': function(done) {
+            var client = qrpc.connect(4245, 'localhost', function(socket) {
+                client.close();
+                done();
+            })
+            // 13k/s
+        },
+**/
+
+/**
+        'new klogClient': function(done) {
+            klog.createClient('testlog', function(err, client) {
+                client.close();
+                done();
+            })
+            // 12k/s
+        },
+**/
+
+        'qrpc w qrpc 1': function(done) {
+            log_100_w_qrpc_qrpc(client, done);
+        },
+        'qrpc w qrpc 2': function(done) {
+            log_100_w_qrpc_qrpc(client, done);
+        },
+        'qrpc w qrpc 3': function(done) {
+            log_100_w_qrpc_qrpc(client, done);
+            // 31 k/s
+        },
+
+        'qrpc w qrpc 1k 1': function(done) {
+            log_1000_w_qrpc_qrpc(client, done);
+        },
+        'qrpc w qrpc 1k 2': function(done) {
+            log_1000_w_qrpc_qrpc(client, done);
+        },
+        'qrpc w qrpc 1k 3': function(done) {
+            log_1000_w_qrpc_qrpc(client, done);
+            // 134 k/s
+        },
+
+        'qrpc w klogClient 1': function(done) {
+            log_100_w_qrpc_klogClient(klogClient, done);
+        },
+        'qrpc w klogClient 2': function(done) {
+            log_100_w_qrpc_klogClient(klogClient, done);
+        },
+        'qrpc w klogClient 3': function(done) {
+            log_100_w_qrpc_klogClient(klogClient, done);
+            // 31 k/s
+        },
+
+        'qrpc w klogClient 100k stream 1': function(done) {
+            log_100k_w_qrpc_klogClient(klogClient, done);
+        },
+        'qrpc w klogClient 100k stream 2': function(done) {
+            log_100k_w_qrpc_klogClient(klogClient, done);
+        },
+        'qrpc w klogClient 100k stream 3': function(done) {
+            log_100k_w_qrpc_klogClient(klogClient, done);
+            // 250 k/s 100k, 190 k/s 10k, 140 k/s 1k lines per sync
+        },
+
+        'express w request 1': function(done) {
+            log_100_w_express_request(done);
+        },
+
+        'express w request 2': function(done) {
+            log_100_w_express_request(done);
+        },
+
+        'express w request 3': function(done) {
+            log_100_w_express_request(done);
+            // 7.2 k/s
+        },
+
+        'express w qhttp 1': function(done) {
+            log_100_w_express_qhttp(done);
+        },
+
+        'express w qhttp 2': function(done) {
+            log_100_w_express_qhttp(done);
+        },
+
+        'express w qhttp 3': function(done) {
+            log_100_w_express_qhttp(done);
+            // 14.8 k/s
+        },
+
+        'restiq w qhttp 1': function(done) {
+            log_100_w_restiq_qhttp(done);
+        },
+
+        'restiq w qhttp 2': function(done) {
+            log_100_w_restiq_qhttp(done);
+        },
+
+        'restiq w qhttp 3': function(done) {
+            log_100_w_restiq_qhttp(done);
+            // 15.8 k/s
+        },
+
+        'restiq w request 1': function(done) {
+            log_100_w_restiq_request(done);
+        },
+
+        'restiq w request 2': function(done) {
+            log_100_w_restiq_request(done);
+        },
+
+        'restiq w request 3': function(done) {
+            log_100_w_restiq_request(done);
+            // 7.8 k/s
+        },
+
+    }, function(err) {
+
+    var client = qrpc.connect(4245, 'localhost', function(socket) {
         // for logging we need to turn off the Nagle algorithm, else only does 40 syncs per second
         socket.setNoDelay();
 
@@ -49,267 +183,6 @@ else {
                 // wait for server to be created
                 setTimeout(next, 100);
             },
-
-            function(next) {
-                console.log("");
-                console.log("express with request");
-                console.time('express');
-                var ndone = 0;
-                for (var i=0; i<10000; i++) {
-                    var uri = {
-                        url: "http://localhost:4246/testlog/write",
-                        body: loglines[i],
-                        agent: httpAgent,
-                    };
-                    request.post(uri, function(err, res, body) {
-                        ndone += 1;
-                        if (ndone == 10000) {
-                            request.post("http://localhost:4246/testlog/sync", function(err, res) {
-                                console.timeEnd('express');
-                                next();
-                            })
-                        }
-                    })
-                }
-            },
-
-            function(next) {
-                console.log("");
-                console.log("express with request");
-                console.time('express');
-                var ndone = 0;
-                for (var i=0; i<10000; i++) {
-                    var uri = {
-                        url: "http://localhost:4246/testlog/write",
-                        body: loglines[i],
-                        agent: httpAgent,
-                    };
-                    request.post(uri, function(err, res, body) {
-                        ndone += 1;
-                        if (ndone == 10000) {
-                            request.post("http://localhost:4246/testlog/sync", function(err, res) {
-                                console.timeEnd('express');
-                                next();
-                            })
-                        }
-                    })
-                }
-            },
-
-            function(next) {
-                console.log("");
-                console.log("restiq with request");
-                console.time('restiq');
-                var ndone = 0;
-                for (var i=0; i<10000; i++) {
-                    var uri = {
-                        url: "http://localhost:4244/testlog/write",
-                        body: loglines[i],
-                        agent: httpAgent,
-                    };
-                    request.post(uri, function(err, res, body) {
-                        ndone += 1;
-                        if (ndone == 10000) {
-                            request.post("http://localhost:4244/testlog/sync", function(err, res) {
-                                console.timeEnd('restiq');
-                                next();
-                            })
-                        }
-                    })
-                }
-            },
-
-            function(next) {
-                console.log("");
-                console.log("restiq with request");
-                console.time('restiq');
-                var ndone = 0;
-                for (var i=0; i<10000; i++) {
-                    var uri = {
-                        url: "http://localhost:4244/testlog/write",
-                        body: loglines[i],
-                        agent: httpAgent,
-                    };
-                    request.post(uri, function(err, res, body) {
-                        ndone += 1;
-                        if (ndone == 10000) {
-                            request.post("http://localhost:4244/testlog/sync", function(err, res) {
-                                console.timeEnd('restiq');
-                                next();
-                            })
-                        }
-                    })
-                }
-            },
-
-            function(next) {
-                console.log("");
-                console.log("restiq with qhttp");
-                console.time('restiq');
-                var ndone = 0;
-                for (var i=0; i<10000; i++) {
-                    var uri = {
-                        url: "http://localhost:4244/testlog/write",
-                        body: loglines[i],
-                        agent: httpAgent,
-                    };
-                    qhttp.post(uri, function(err, res, body) {
-                        ndone += 1;
-                        if (ndone == 10000) {
-                            qhttp.post("http://localhost:4244/testlog/sync", function(err, res) {
-                                console.timeEnd('restiq');
-                                next();
-                            })
-                        }
-                    })
-                }
-            },
-
-            function(next) {
-                console.log("");
-                console.log("restiq with qhttp");
-                console.time('restiq');
-                var ndone = 0;
-                for (var i=0; i<10000; i++) {
-                    var uri = {
-                        url: "http://localhost:4244/testlog/write",
-                        body: loglines[i],
-                        agent: httpAgent,
-                    };
-                    qhttp.post(uri, function(err, res, body) {
-                        ndone += 1;
-                        if (ndone == 10000) {
-                            qhttp.post("http://localhost:4244/testlog/sync", function(err, res) {
-                                console.timeEnd('restiq');
-                                next();
-                            })
-                        }
-                    })
-                }
-            },
-
-    ///**
-
-            function(next) {
-                console.log("");
-                console.log("express with qhttp");
-                console.time('express');
-                var ndone = 0;
-                for (var i=0; i<10000; i++) {
-                    var uri = {
-                        method: 'POST',
-                        url: "http://localhost:4246/testlog/write",
-                        body: loglines[i],
-                        agent: httpAgent,
-                    };
-                    qhttp.post(uri, function(err, res, body) {
-                        ndone += 1;
-                        if (ndone == 10000) {
-                            qhttp.post("http://localhost:4246/testlog/sync", function(err, res) {
-                                console.timeEnd('express');
-                                next();
-                            })
-                        }
-                    })
-                }
-            },
-
-            function(next) {
-                console.log("");
-                console.log("express with qhttp");
-                console.time('express');
-                var ndone = 0;
-                for (var i=0; i<10000; i++) {
-                    var uri = {
-                        method: 'POST',
-                        url: "http://localhost:4246/testlog/write",
-                        body: loglines[i],
-                        agent: httpAgent,
-                    };
-                    qhttp.post(uri, function(err, res, body) {
-                        ndone += 1;
-                        if (ndone == 10000) {
-                            qhttp.post("http://localhost:4246/testlog/sync", function(err, res) {
-                                console.timeEnd('express');
-                                next();
-                            })
-                        }
-                    })
-                }
-            },
-
-            function(next) {
-                console.log("");
-                console.log("express with qhttp");
-                console.time('express');
-                var ndone = 0;
-                for (var i=0; i<10000; i++) {
-                    var uri = {
-                        method: 'POST',
-                        url: "http://localhost:4246/testlog/write",
-                        body: loglines[i],
-                        agent: httpAgent,
-                    };
-                    qhttp.post(uri, function(err, res, body) {
-                        ndone += 1;
-                        if (ndone == 10000) {
-                            qhttp.post("http://localhost:4246/testlog/sync", function(err, res) {
-                                console.timeEnd('express');
-                                next();
-                            })
-                        }
-                    })
-                }
-            },
-
-            function(next) {
-                console.log("");
-                console.log("restiq with qhttp");
-                console.time('restiq');
-                var ndone = 0;
-                for (var i=0; i<10000; i++) {
-                    var uri = {
-                        method: 'POST',
-                        url: "http://localhost:4244/testlog/write",
-                        body: loglines[i],
-                        agent: httpAgent,
-                    };
-                    qhttp.post(uri, function(err, res, body) {
-                        ndone += 1;
-                        if (ndone == 10000) {
-                            qhttp.post("http://localhost:4244/testlog/sync", function(err, res) {
-                                console.timeEnd('restiq');
-                                next();
-                            })
-                        }
-                    })
-                }
-            },
-
-            function(next) {
-                console.log("");
-                console.log("restiq with qhttp");
-                console.time('restiq');
-                var ndone = 0;
-                for (var i=0; i<10000; i++) {
-                    var uri = {
-                        url: "http://localhost:4244/testlog/write",
-                        body: loglines[i],
-                        agent: httpAgent,
-                    };
-                    qhttp.post(uri, function(err, res, body) {
-                        ndone += 1;
-                        if (ndone == 10000) {
-                            qhttp.post("http://localhost:4246/testlog/sync", function(err, res) {
-                                console.timeEnd('restiq');
-                                next();
-                            })
-                        }
-                    })
-                }
-            },
-
-/**/
 
             function(next) {
                 console.log("");
@@ -373,4 +246,129 @@ else {
         });
 
     })
+
+    })
+}
+
+function log_100_w_express_request( done ) {
+    var nloops = 1000;
+    var ndone = 0;
+
+    for (var i=0; i<nloops; i++) {
+        var uri = {
+            url: "http://localhost:4246/testlog/write",
+            body: loglines[i],
+            agent: httpAgent,
+        };
+        request.post(uri, whenDone);
+    }
+
+    function whenDone(err, res, body) {
+        ndone += 1;
+        if (ndone === nloops) {
+            request.post("http://localhost:4246/testlog/sync", function(err, res) {
+// FIXME: RangeError: Maximum call stack size exceeded in qtimeit ?!
+// works @ 100
+// ... could not reproduce
+                done();
+            })
+        }
+    }
+}
+
+function log_100_w_express_qhttp( done ) {
+    var nloops = 100;
+    var ndone = 0;
+
+    for (var i=0; i<nloops; i++) {
+        var uri = {
+            url: "http://localhost:4246/testlog/write",
+            body: loglines[i],
+            agent: httpAgent,
+        };
+        qhttp.post(uri, whenDone);
+    }
+
+    function whenDone(err, res, body) {
+        ndone += 1;
+        if (ndone === nloops) {
+            qhttp.post("http://localhost:4246/testlog/sync", done);
+        }
+    }
+}
+
+function log_100_w_restiq_qhttp( done ) {
+    var nloops = 100;
+    var ndone = 0;
+
+    for (var i=0; i<nloops; i++) {
+        var uri = {
+            url: "http://localhost:4244/testlog/write",
+            body: loglines[i],
+            agent: httpAgent,
+        };
+        qhttp.post(uri, whenDone);
+    }
+
+    function whenDone(err, res, body) {
+        ndone += 1;
+        if (ndone === nloops) {
+            qhttp.post("http://localhost:4244/testlog/sync", done);
+        }
+    }
+}
+
+function log_100_w_restiq_request( done ) {
+    var nloops = 100;
+    var ndone = 0;
+
+    for (var i=0; i<nloops; i++) {
+        var uri = {
+            url: "http://localhost:4244/testlog/write",
+            body: loglines[i],
+            agent: httpAgent,
+        };
+        request.post(uri, whenDone);
+    }
+
+    function whenDone(err, res, body) {
+        ndone += 1;
+        if (ndone === nloops) {
+            request.post("http://localhost:4244/testlog/sync", done);
+        }
+    }
+}
+
+function log_100_w_qrpc_qrpc( client, done ) {
+    for (var i=0; i<100; i++) {
+        client.call('write', loglines[i]);
+    }
+    client.call('sync', function(err, ret) {
+        done();
+    })
+}
+
+function log_1000_w_qrpc_qrpc( client, done ) {
+    for (var i=0; i<1000; i++) {
+        client.call('write', loglines[i]);
+    }
+    client.call('sync', function(err, ret) {
+        done();
+    })
+}
+
+function log_100_w_qrpc_klogClient( klogClient, done ) {
+    for (var i=0; i<100; i++) {
+        klogClient.write(loglines[i]);
+    }
+    client.call('sync', function(err, ret) {
+        done();
+    })
+}
+
+function log_100k_w_qrpc_klogClient( klogClient, done ) {
+    for (var i=0; i<100000; i++) {
+        klogClient.write(loglines[i]);
+    }
+    klogClient.fflush(done);
 }
